6ab0db182048b142b2f3811520ecbbba0272b304
Jorgetorreslopez <usda4ever@gmail.com>
2023-05-30 21:45:19 -0400

    Working on using MongoDB to Generate ID

--- Express.js
+++ Express.js
@@ -1,49 +1,40 @@
-//require('dotenv').config()
+require('dotenv').config()
 const express = require('express')
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 // const jsxEngine = require('jsx-view-engine')
-// const Item = require('./models/item')
+const Item = require('./models/item')
 const PORT = process.env.PORT || 3000
 
 const app = express()
 
 app.use(express.json())
 
-// mongoose.connect(process.env.MONGO_URI)
-// mongoose.connection.once('open', () => {
-//     console.log('connected to mongodb')
-// })
+mongoose.connect(process.env.MONGO_URI)
+mongoose.connection.once('open', () => {
+    console.log('mongo!')
+})
 
 let items = []
 
-// I.N.D.U.C.E.S. //
-
-// INDEX - A TABLE OF CONTENTS OF ALL YOUR RESOURCES
 app.get('/items', (req, res) => {
     res.json(items)
 })
 
-
-// NEW
-// show the user a form to fill out to create
-
-
-// DELETE
-// backend only functionality that is used to delete
-
-// UPDATE
-// backend only functionality that is used to update
-
-// CREATE
-// backend only functionality that is used to create
-
-// EDIT
-// show you a form that lets you edit t
-
-// SHOW
-// shows you 1 individual 
-
+app.post('/items', (req, res) => {
+    const { name, price } = req.body
+    const newItem = new Item({
+        name,
+        price
+    })
+    newItem.save()
+    .then((savedItem) => {
+      res.status(201).json(savedItem);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'Failed to add item' });
+    });
+})
 
 app.listen(PORT, () => {
-    console.log(`${PORT} initiated!`)
+    console.log(`Andre ${PORT} da Goat!`)
 })
--- models/item.js
+++ models/item.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose')
 
 const itemSchema = new mongoose.Schema({
-    
+    name: { type: String, require: true},
+    Price: { type: Number, require: true}
 })
+
+const Item = mongoose.model('Item', itemSchema)
+
+module.exports = Item
